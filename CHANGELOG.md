d11416cca45753b831cf6ff43cd19bcb9da3af1d
DanielK <saponembigas@gmail.com>
2025-09-15 21:57:20 +0700

    feat(et-1): Exceptions models add

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.0.1 (15.09.2025)
+[et-1]
+* Добавление классов ошибок
+  BaseGlobalExceptionHandler,
+  ErrorResponse,
+  ErrorResponseException,
+  ErrorStatus,
+  Service;
---
+++ src/main/java/com/kuklin/sharedlibrary/exceptions/Service.java
@@ -0,0 +1,10 @@
+package com.kuklin.sharedlibrary.exceptions;
+
+public enum Service {
+    USER_SERVICE,
+    AI_CONVERSATION_SERVICE,
+    INTERVIEW_SERVICE,
+    TGBOT_SERVICE,
+    AUTH_SERVICE
+    ;
+}
